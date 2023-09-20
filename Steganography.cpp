298feedf40e59b939452e91946f4ccb1e1d1c9b6
Jesse Doescher <jdoesche@ut.utm.edu>
2023-09-20 10:13:25 -0500

    Layout for the CPP File

---
+++ Steganography.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+void Steganography::readImage(string fileName)
+{
+
+}
+
+void Steganography::printImage(string fileName)
+{
+
+}
+
+void Steganography::readCipherText(string fileName)
+{
+
+}
+
+void Steganography::cleanImage()
+{
+
+}
+
+void Steganography::encipher()
+{
+
+}
+
+void Steganography::decipher()
+{
+
+}
