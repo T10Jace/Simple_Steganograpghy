feb61c4958d278285fc2c8604f83b1f2071551b5
Jesse Doescher <jdoesche@ut.utm.edu>
2023-09-25 11:29:04 -0500

    What on earth

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 #include <fstream>
 #include <vector>
-#include "STEGONOGRAPHY_H";
+#include "Steganography.h";
 
 int main()
 {
     cout << "Welcome to the Secret Message Reader program!" << endl;
     Steganography a;
+
+    string filename;
+
+    cout << "Enter a file name " << endl;
+    cin >> filename;
+
+    a.printCipherText(filename);
 
     return 0;
 }
